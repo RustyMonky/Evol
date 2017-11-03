8b86ee1d76f84c3d5ac7b71df274702be8ff7783
Ian <ian.cantor@fandm.edu>
2017-11-03 00:17:44 -0400

    Reworks is moving detection, Implements grid-based player movement

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,8 +1,14 @@
 extends KinematicBody2D
 
 var direction = Vector2()
+var grid
 var speed = 0
-var velocity = 0
+var type
+var velocity = Vector2()
+
+var is_moving = false
+var target_pos = Vector2()
+var target_direction = Vector2()
 
 const TOP = Vector2(0, -1)
 const DOWN = Vector2(0, 1)
@@ -14,28 +20,43 @@ const MAX_SPEED = 200
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
+	grid = get_parent()
+	type = grid.PLAYER
 	set_fixed_process(true)
 
 func _fixed_process(delta):
 	# Movement
-	var is_moving = Input.is_action_pressed("player_moving")
+	direction = Vector2()
 
-	if is_moving:
+	if Input.is_action_pressed("player_up"):
+		direction = TOP
+		get_node("PlayerSprite").set_frame(1)
+	elif Input.is_action_pressed("player_down"):
+		direction = DOWN
+		get_node("PlayerSprite").set_frame(0)
+	elif Input.is_action_pressed("player_left"):
+		direction = LEFT
+		get_node("PlayerSprite").set_frame(3)
+	elif Input.is_action_pressed("player_right"):
+		direction = RIGHT
+		get_node("PlayerSprite").set_frame(2)
+
+	if not is_moving and direction != Vector2():
+		target_direction = direction
+		if grid.is_cell_vacant(get_pos(), target_direction):
+			target_pos = grid.update_child_pos(self)
+			is_moving = true
+	elif is_moving:
 		speed = MAX_SPEED
-		if Input.is_action_pressed("player_up"):
-			direction = TOP
-			get_node("PlayerSprite").set_frame(1)
-		elif Input.is_action_pressed("player_down"):
-			direction = DOWN
-			get_node("PlayerSprite").set_frame(0)
-		elif Input.is_action_pressed("player_left"):
-			direction = LEFT
-			get_node("PlayerSprite").set_frame(3)
-		elif Input.is_action_pressed("player_right"):
-			direction = RIGHT
-			get_node("PlayerSprite").set_frame(2)
-	else:
-		speed = 0
+		velocity = speed * target_direction * delta
+		move(velocity)
+		
+		var pos = get_pos()
+		var distance_to_target = Vector2(abs(target_pos.x - pos.x), abs(target_pos.y - pos.y))
 	
-	velocity = speed * direction * delta
-	move(velocity)
+		if abs(velocity.x) > distance_to_target.x:
+			velocity.x = distance_to_target.x * target_direction.x
+			is_moving = false
+		elif abs(velocity.y) > distance_to_target.y:
+			velocity.y = distance_to_target.y * target_direction.y
+			is_moving = false
