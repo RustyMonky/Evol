97fe2a41a2f48bd54048c3c003d7a5def849bb36
Ian <ian.cantor@fandm.edu>
2018-07-02 23:24:21 -0400

    Fixes typo with saving stat sprite upon evolution

--- Battle/evolution/Evolution.gd
+++ Battle/evolution/Evolution.gd
@@ -66,32 +66,32 @@ func choose_form():
 	if evolutions[current_choice].name == 'Defense':
 		gameData.player.form = "defense"
 		gameData.player.battle_sprite = "res://assets/sprites/forms/backs/baseDefBack.png"
-		gameData.player.stat_sprite = "res://assets/sprites/forms/sheets-48x48/baseDefSheet.png"
+		gameData.player.stats_sprite = "res://assets/sprites/forms/sheets-48x48/baseDefSheet.png"
 		gameData.player.sprite_path = "res://assets/sprites/forms/sheets-48x48/baseDefSheet.png"
 	elif evolutions[current_choice].name == 'Speed':
 		gameData.player.form = "speed"
 		gameData.player.battle_sprite = "res://assets/sprites/forms/backs/baseSpdBack.png"
-		gameData.player.stat_sprite = "res://assets/sprites/forms/sheets-48x48/baseSpdSheet.png"
+		gameData.player.stats_sprite = "res://assets/sprites/forms/sheets-48x48/baseSpdSheet.png"
 		gameData.player.sprite_path = "res://assets/sprites/forms/sheets-48x48/baseSpdSheet.png"
 	elif evolutions[current_choice].name == 'Strength':
 		gameData.player.form = "strength"
 		gameData.player.battle_sprite = "res://assets/sprites/forms/backs/baseStrBack.png"
-		gameData.player.stat_sprite = "res://assets/sprites/forms/sheets-48x48/baseStrSheet.png"
+		gameData.player.stats_sprite = "res://assets/sprites/forms/sheets-48x48/baseStrSheet.png"
 		gameData.player.sprite_path = "res://assets/sprites/forms/sheets-48x48/baseStrSheet.png"
 	elif evolutions[current_choice].name == 'Fire':
 		gameData.player.elemental_type = "fire"
 		gameData.player.battle_sprite = "res://assets/sprites/forms/backs/baseFireBack.png"
-		gameData.player.stat_sprite = "res://assets/sprites/forms/sheets-48x48/baseFireSheet.png"
+		gameData.player.stats_sprite = "res://assets/sprites/forms/sheets-48x48/baseFireSheet.png"
 		gameData.player.sprite_path = "res://assets/sprites/forms/sheets-48x48/baseFireSheet.png"
 	elif evolutions[current_choice].name == 'Water':
 		gameData.player.elemental_type = "water"
 		gameData.player.battle_sprite = "res://assets/sprites/forms/backs/baseWaterBack.png"
-		gameData.player.stat_sprite = "res://assets/sprites/forms/sheets-48x48/baseWaterSheet.png"
+		gameData.player.stats_sprite = "res://assets/sprites/forms/sheets-48x48/baseWaterSheet.png"
 		gameData.player.sprite_path = "res://assets/sprites/forms/sheets-48x48/baseWaterSheet.png"
 	elif evolutions[current_choice].name == 'Plant':
 		gameData.player.elemental_type = "plant"
 		gameData.player.battle_sprite = "res://assets/sprites/forms/backs/baseGrassBack.png"
-		gameData.player.stat_sprite = "res://assets/sprites/forms/sheets-48x48/baseGrassSheet.png"
+		gameData.player.stats_sprite = "res://assets/sprites/forms/sheets-48x48/baseGrassSheet.png"
 		gameData.player.sprite_path = "res://assets/sprites/forms/sheets-48x48/baseGrassSheet.png"
 
 	$vBox/confirmLabel.set_text("You've evolved into the " + evolutions[current_choice].name + " form!")
